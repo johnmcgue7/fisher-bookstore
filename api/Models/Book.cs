4e767f6b60e969a296dcc5ed0a3bba3c345e7fea
John McGue <JohnMcGue@Johns-MacBook-Air.local>
2019-02-20 15:27:42 -0500

    newest commit

---
+++ api/Models/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
