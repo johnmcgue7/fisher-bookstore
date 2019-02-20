92b71d74f772620ff270a695e54c82a58cdf4cfe
John McGue <JohnMcGue@Johns-MacBook-Air.local>
2019-02-20 15:59:31 -0500

    get all books

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -5,8 +5,10 @@ namespace Fisher.Bookstore.Models
     
     public class Book
     {
-        public int BookId { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN {get; set; } 
         
     }
 }
